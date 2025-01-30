ad151f78c06b29268e18b35e551a6d4aed2ae5bf
MagnusKomet <ehosta.dam@institutcampalans.net>
2025-01-30 13:33:52 +0100

    Player prototype

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,49 +1,209 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    float speed = 0;
+    
+    PlayerInput playerInput;
+    PlayerInput.OnFootActions input;
 
-    Rigidbody rb;
+    CharacterController controller;
+    Animator animator;
+    AudioSource audioSource;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
+    [Header("Controller")]
+    public float moveSpeed = 5;
+    public float gravity = -9.8f;
+    public float jumpHeight = 1.2f;
+
+    Vector3 _PlayerVelocity;
+
+    bool isGrounded;
+
+    [Header("Camera")]
+    public Camera cam;
+    public float sensitivity;
+
+    float xRotation = 0f;
+
+    void Awake()
+    { 
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        playerInput = new PlayerInput();
+        input = playerInput.OnFoot;
+        AssignInputs();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-        rb.AddForce(movement * speed * Time.deltaTime);
+        isGrounded = controller.isGrounded;
+
+        // Repeat Inputs
+        if(input.Attack.IsPressed())
+        { Attack(); }
+
+        SetAnimations();
     }
 
-    private void OnTriggerEnter(Collider collision)
+    void FixedUpdate() 
+    { MoveInput(input.Movement.ReadValue<Vector2>()); }
+
+    void LateUpdate() 
+    { LookInput(input.Look.ReadValue<Vector2>()); }
+
+    void MoveInput(Vector2 input)
     {
-        if (collision.gameObject.CompareTag("PickUp"))
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+
+        controller.Move(transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+        _PlayerVelocity.y += gravity * Time.deltaTime;
+        if(isGrounded && _PlayerVelocity.y < 0)
+            _PlayerVelocity.y = -2f;
+        controller.Move(_PlayerVelocity * Time.deltaTime);
+    }
+
+    void LookInput(Vector3 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        xRotation -= (mouseY * Time.deltaTime * sensitivity);
+        xRotation = Mathf.Clamp(xRotation, -80, 80);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime * sensitivity));
+    }
+
+    void OnEnable() 
+    { input.Enable(); }
+
+    void OnDisable()
+    { input.Disable(); }
+
+    void Jump()
+    {
+        // Adds force to the player rigidbody to jump
+        if (isGrounded)
+            _PlayerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+    }
+
+    void AssignInputs()
+    {
+        input.Jump.performed += ctx => Jump();
+        input.Attack.started += ctx => Attack();
+    }
+
+    // ---------- //
+    // ANIMATIONS //
+    // ---------- //
+
+    public const string IDLE = "Idle";
+    public const string WALK = "Walk";
+    public const string ATTACK1 = "Attack 1";
+    public const string ATTACK2 = "Attack 2";
+
+    string currentAnimationState;
+
+    public void ChangeAnimationState(string newState) 
+    {
+        // STOP THE SAME ANIMATION FROM INTERRUPTING WITH ITSELF //
+        if (currentAnimationState == newState) return;
+
+        // PLAY THE ANIMATION //
+        currentAnimationState = newState;
+        animator.CrossFadeInFixedTime(currentAnimationState, 0.2f);
+    }
+
+    void SetAnimations()
+    {
+        // If player is not attacking
+        if(!attacking)
         {
-            Destroy(collision.gameObject);
-        }
-        if (collision.gameObject.CompareTag("WorldBorder"))
-        {
-            rb.transform.position = new Vector3(0, 0.5f, 0);
-            rb.velocity = Vector3.zero;
+            if(_PlayerVelocity.x == 0 &&_PlayerVelocity.z == 0)
+            { ChangeAnimationState(IDLE); }
+            else
+            { ChangeAnimationState(WALK); }
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            rb.AddForce(Vector3.up * 30000 * Time.deltaTime);
-        }
+    // ------------------- //
+    // ATTACKING BEHAVIOUR //
+    // ------------------- //
 
+    [Header("Attacking")]
+    public float attackDistance = 3f;
+    public float attackDelay = 0.4f;
+    public float attackSpeed = 1f;
+    public int attackDamage = 1;
+    public LayerMask attackLayer;
+
+    public GameObject hitEffect;
+    public AudioClip swordSwing;
+    public AudioClip hitSound;
+
+    bool attacking = false;
+    bool readyToAttack = true;
+    int attackCount;
+
+    public void Attack()
+    {
+        if(!readyToAttack || attacking) return;
+
+        readyToAttack = false;
+        attacking = true;
+
+        Invoke(nameof(ResetAttack), attackSpeed);
+        Invoke(nameof(AttackRaycast), attackDelay);
+
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+        audioSource.PlayOneShot(swordSwing);
+
+        if(attackCount == 0)
+        {
+            ChangeAnimationState(ATTACK1);
+            attackCount++;
+        }
+        else
+        {
+            ChangeAnimationState(ATTACK2);
+            attackCount = 0;
+        }
+    }
+
+    void ResetAttack()
+    {
+        attacking = false;
+        readyToAttack = true;
+    }
+
+    void AttackRaycast()
+    {
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, attackDistance, attackLayer))
+        { 
+            HitTarget(hit.point);
+
+            if(hit.transform.TryGetComponent<Actor>(out Actor T))
+            { T.TakeDamage(attackDamage); }
+        } 
+    }
+
+    void HitTarget(Vector3 pos)
+    {
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+        audioSource.PlayOneShot(hitSound);
+
+        GameObject GO = Instantiate(hitEffect, pos, Quaternion.identity);
+        Destroy(GO, 20);
     }
 }
-
-
