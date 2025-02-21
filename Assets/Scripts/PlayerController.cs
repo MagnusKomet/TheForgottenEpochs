43a8f61430bab23d5e71ab54402f1ff3b1784d26
MagnusKomet <ehosta.dam@institutcampalans.net>
2025-02-21 13:33:04 +0100

    UI changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,302 +1,311 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour
+namespace PlayerSpace
 {
-
-    // ---------- //
-    //   General  //
-    // ---------- //
-
-    #region General
-
-    PlayerInput playerInput;
-    PlayerInput.OnFootActions input;
-
-    [SerializeField]
-    GameObject shortParticleOnHit;
-    [SerializeField]
-    GameObject longParticleOnHit;
-
-    CharacterController controller;
-    Animator animator;
-    AudioSource audioSource;
-
-    [Header("Controller")]
-    public float moveSpeed = 5;
-    public float gravity = -9.8f;
-    public float jumpHeight = 1.2f;
-
-    Vector3 _PlayerVelocity;
-
-    bool isGrounded;
-
-    [Header("Camera")]
-    public Camera cam;
-    public float sensitivity;
-
-    float xRotation = 0f;
-
-    private string combo = "";
-
-    void Awake()
-    { 
-        controller = GetComponent<CharacterController>();
-        animator = GetComponentInChildren<Animator>();
-        audioSource = GetComponent<AudioSource>();
-
-        playerInput = new PlayerInput();
-        input = playerInput.OnFoot;
-        AssignInputs();
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    void Update()
+    public class PlayerController : MonoBehaviour
     {
-        isGrounded = controller.isGrounded;
 
+        // ---------- //
+        //   General  //
+        // ---------- //
 
-        if (input.Earth.WasReleasedThisFrame())
+        #region General
+
+        PlayerInput playerInput;
+        PlayerInput.OnFootActions input;
+
+        [SerializeField]
+        GameObject shortParticleOnHit;
+        [SerializeField]
+        GameObject longParticleOnHit;
+
+        [SerializeField]
+        ManaMeterScript FireManaMeter;
+
+        CharacterController controller;
+        Animator animator;
+        AudioSource audioSource;
+
+        [Header("Controller")]
+        public float moveSpeed = 5;
+        public float gravity = -9.8f;
+        public float jumpHeight = 1.2f;
+
+        Vector3 _PlayerVelocity;
+
+        bool isGrounded;
+
+        [Header("Camera")]
+        public Camera cam;
+        public float sensitivity;
+
+        float xRotation = 0f;
+
+        private string combo = "";
+
+        void Awake()
         {
-            combo += "T";
-        }
-        else if (input.Wind.WasReleasedThisFrame())
-        {
-            combo += "A";
-        }
-        else if (input.Fire.WasReleasedThisFrame())
-        {
-            combo += "Z";
-        }
-        else if (input.Water.WasReleasedThisFrame())
-        {
-            combo += "K";
+            controller = GetComponent<CharacterController>();
+            animator = GetComponentInChildren<Animator>();
+            audioSource = GetComponent<AudioSource>();
+
+            playerInput = new PlayerInput();
+            input = playerInput.OnFoot;
+            AssignInputs();
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
 
-        // Repeat Inputs
-        if (input.Attack.IsPressed())
-        { Attack(); }
-
-        SetAnimations();
-    }
-
-    void FixedUpdate() 
-    { MoveInput(input.Movement.ReadValue<Vector2>()); }
-
-    void LateUpdate() 
-    { LookInput(input.Look.ReadValue<Vector2>()); }
-
-    void MoveInput(Vector2 input)
-    {
-        Vector3 moveDirection = Vector3.zero;
-        moveDirection.x = input.x;
-        moveDirection.z = input.y;
-
-        controller.Move(transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
-        _PlayerVelocity.y += gravity * Time.deltaTime;
-        if(isGrounded && _PlayerVelocity.y < 0)
-            _PlayerVelocity.y = -2f;
-        controller.Move(_PlayerVelocity * Time.deltaTime);
-    }
-
-    void LookInput(Vector3 input)
-    {
-        float mouseX = input.x;
-        float mouseY = input.y;
-
-        xRotation -= (mouseY * Time.deltaTime * sensitivity);
-        xRotation = Mathf.Clamp(xRotation, -80, 80);
-
-        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-
-        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime * sensitivity));
-    }
-
-    void OnEnable() 
-    { input.Enable(); }
-
-    void OnDisable()
-    { input.Disable(); }
-
-    void Jump()
-    {
-        // Adds force to the player rigidbody to jump
-        if (isGrounded)
-            _PlayerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
-    }
-
-    void AssignInputs()
-    {
-        input.Jump.performed += ctx => Jump();
-        input.Attack.started += ctx => Attack();
-    }
-
-    #endregion
-
-    // ---------- //
-    // ANIMATIONS //
-    // ---------- //
-
-    #region Animations
-    public const string IDLE = "Idle";
-    public const string WALK = "Walk";
-    public const string ATTACK1 = "Attack 1";
-    public const string ATTACK2 = "Attack 2";
-    public const string ATTACKORB = "Attack Orb";
-
-    string currentAnimationState;
-
-    public void ChangeAnimationState(string newState) 
-    {
-        // STOP THE SAME ANIMATION FROM INTERRUPTING WITH ITSELF //
-        if (currentAnimationState == newState) return;
-
-        // PLAY THE ANIMATION //
-        currentAnimationState = newState;
-        animator.CrossFadeInFixedTime(currentAnimationState, 0.2f);
-    }
-
-    void SetAnimations()
-    {
-        // If player is not attacking
-        if(!attacking)
+        void Update()
         {
-            if(_PlayerVelocity.x == 0 &&_PlayerVelocity.z == 0)
-            { ChangeAnimationState(IDLE); }
+            isGrounded = controller.isGrounded;
+
+
+            if (input.Earth.WasReleasedThisFrame())
+            {
+                combo += "T";
+            }
+            else if (input.Wind.WasReleasedThisFrame())
+            {
+                combo += "A";
+            }
+            else if (input.Fire.WasReleasedThisFrame())
+            {
+                if (FireManaMeter.ReduceMana())
+                {
+                    combo += "Z";
+                }
+            }
+            else if (input.Water.WasReleasedThisFrame())
+            {
+                combo += "K";
+            }
+
+            // Repeat Inputs
+            if (input.Attack.IsPressed())
+            { Attack(); }
+
+            SetAnimations();
+        }
+
+        void FixedUpdate()
+        { MoveInput(input.Movement.ReadValue<Vector2>()); }
+
+        void LateUpdate()
+        { LookInput(input.Look.ReadValue<Vector2>()); }
+
+        void MoveInput(Vector2 input)
+        {
+            Vector3 moveDirection = Vector3.zero;
+            moveDirection.x = input.x;
+            moveDirection.z = input.y;
+
+            controller.Move(transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+            _PlayerVelocity.y += gravity * Time.deltaTime;
+            if (isGrounded && _PlayerVelocity.y < 0)
+                _PlayerVelocity.y = -2f;
+            controller.Move(_PlayerVelocity * Time.deltaTime);
+        }
+
+        void LookInput(Vector3 input)
+        {
+            float mouseX = input.x;
+            float mouseY = input.y;
+
+            xRotation -= (mouseY * Time.deltaTime * sensitivity);
+            xRotation = Mathf.Clamp(xRotation, -80, 80);
+
+            cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+            transform.Rotate(Vector3.up * (mouseX * Time.deltaTime * sensitivity));
+        }
+
+        void OnEnable()
+        { input.Enable(); }
+
+        void OnDisable()
+        { input.Disable(); }
+
+        void Jump()
+        {
+            // Adds force to the player rigidbody to jump
+            if (isGrounded)
+                _PlayerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+
+        void AssignInputs()
+        {
+            input.Jump.performed += ctx => Jump();
+            input.Attack.started += ctx => Attack();
+        }
+
+        #endregion
+
+        // ---------- //
+        // ANIMATIONS //
+        // ---------- //
+
+        #region Animations
+        public const string IDLE = "Idle";
+        public const string WALK = "Walk";
+        public const string ATTACK1 = "Attack 1";
+        public const string ATTACK2 = "Attack 2";
+        public const string ATTACKORB = "Attack Orb";
+
+        string currentAnimationState;
+
+        public void ChangeAnimationState(string newState)
+        {
+            // STOP THE SAME ANIMATION FROM INTERRUPTING WITH ITSELF //
+            if (currentAnimationState == newState) return;
+
+            // PLAY THE ANIMATION //
+            currentAnimationState = newState;
+            animator.CrossFadeInFixedTime(currentAnimationState, 0.2f);
+        }
+
+        void SetAnimations()
+        {
+            // If player is not attacking
+            if (!attacking)
+            {
+                if (_PlayerVelocity.x == 0 && _PlayerVelocity.z == 0)
+                { ChangeAnimationState(IDLE); }
+                else
+                { ChangeAnimationState(WALK); }
+            }
+        }
+
+        #endregion
+
+        // ------------------- //
+        // ATTACKING BEHAVIOUR //
+        // ------------------- //
+
+        #region Attacks
+
+        [Header("Attacking")]
+        public float attackDistance = 3f;
+        public float attackDelay = 0.4f;
+        public float attackSpeed = 1f;
+        public int attackDamage = 1;
+
+        [SerializeField]
+        private GameObject fireball;
+        [SerializeField]
+        private GameObject secondFireball;
+
+        [SerializeField]
+        private Transform SpellsSpawnPoint;
+
+        public AudioClip swordSwing;
+        public AudioClip hitSound;
+
+        bool attacking = false;
+        bool readyToAttack = true;
+        int attackCount;
+
+        public void Attack()
+        {
+            if (!readyToAttack || attacking) return;
+
+            readyToAttack = false;
+            attacking = true;
+
+            Invoke(nameof(ResetAttack), attackSpeed);
+
+            if (combo.Length > 0)
+            {
+                Invoke(nameof(ShootSpell), attackDelay);
+            }
             else
-            { ChangeAnimationState(WALK); }
-        }
-    }
+            {
+                Invoke(nameof(ShootFireball), attackDelay);
+            }
 
-    #endregion
+            audioSource.pitch = Random.Range(0.9f, 1.1f);
+            audioSource.PlayOneShot(swordSwing);
 
-    // ------------------- //
-    // ATTACKING BEHAVIOUR //
-    // ------------------- //
+            if (attackCount == 0)
+            {
+                ChangeAnimationState(ATTACK1);
+                attackCount++;
+            }
+            else if (attackCount == 1)
+            {
+                ChangeAnimationState(ATTACKORB);
+                attackCount++;
+            }
+            else
+            {
+                ChangeAnimationState(ATTACK2);
+                attackCount = 0;
+            }
 
-    #region Attacks
-
-    [Header("Attacking")]
-    public float attackDistance = 3f;
-    public float attackDelay = 0.4f;
-    public float attackSpeed = 1f;
-    public int attackDamage = 1;
-
-    [SerializeField]
-    private GameObject fireball;
-    [SerializeField]
-    private GameObject secondFireball;
-
-    [SerializeField]
-    private Transform SpellsSpawnPoint;
-
-    public AudioClip swordSwing;
-    public AudioClip hitSound;
-
-    bool attacking = false;
-    bool readyToAttack = true;
-    int attackCount;
-
-    public void Attack()
-    {
-        if (!readyToAttack || attacking) return;
-
-        readyToAttack = false;
-        attacking = true;
-
-        Invoke(nameof(ResetAttack), attackSpeed);
-
-        if (combo.Length > 0)
-        {
-            Invoke(nameof(ShootSpell), attackDelay);
-        }
-        else
-        {
-            Invoke(nameof(ShootFireball), attackDelay);
         }
 
-        audioSource.pitch = Random.Range(0.9f, 1.1f);
-        audioSource.PlayOneShot(swordSwing);
-
-        if(attackCount == 0)
+        void ResetAttack()
         {
-            ChangeAnimationState(ATTACK1);
-            attackCount++;
-        }
-        else if(attackCount == 1)
-        {
-            ChangeAnimationState(ATTACKORB);
-            attackCount++;
-        }
-        else
-        {
-            ChangeAnimationState(ATTACK2);
-            attackCount = 0;
+            attacking = false;
+            readyToAttack = true;
         }
 
-    }
-
-    void ResetAttack()
-    {
-        attacking = false;
-        readyToAttack = true;
-    }
-
-    void ShootFireball()
-    {
-        var ball = Instantiate(fireball, SpellsSpawnPoint.position, SpellsSpawnPoint.rotation);
-        Rigidbody rb = ball.GetComponent<Rigidbody>();
-        if (rb != null)
+        void ShootFireball()
         {
-            rb.velocity = cam.transform.forward * 50f;
-        }
-        Destroy(ball, 30f); // Destroy the fireball after 30 seconds
-    }
-
-    void ShootSpell()
-    {
-        if(combo == "ZZA")
-        {
-            Debug.Log("Fireball");
-            var ball = Instantiate(secondFireball, SpellsSpawnPoint.position, SpellsSpawnPoint.rotation);
+            var ball = Instantiate(fireball, SpellsSpawnPoint.position, SpellsSpawnPoint.rotation);
             Rigidbody rb = ball.GetComponent<Rigidbody>();
             if (rb != null)
             {
                 rb.velocity = cam.transform.forward * 50f;
             }
-            Destroy(ball, 30f);
+            Destroy(ball, 30f); // Destroy the fireball after 30 seconds
         }
-        else
+
+        void ShootSpell()
         {
-            Debug.Log("Combo failed: " + combo);
+            if (combo == "ZZA")
+            {
+                Debug.Log("Fireball");
+                var ball = Instantiate(secondFireball, SpellsSpawnPoint.position, SpellsSpawnPoint.rotation);
+                Rigidbody rb = ball.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.velocity = cam.transform.forward * 50f;
+                }
+                Destroy(ball, 30f);
+            }
+            else
+            {
+                Debug.Log("Combo failed: " + combo);
+            }
+            combo = "";
         }
-        combo = "";
-    }
 
-    #endregion
+        #endregion
 
-    // ------------------- //
-    //      COLLISIONS     //
-    // ------------------- //
+        // ------------------- //
+        //      COLLISIONS     //
+        // ------------------- //
 
-    #region Collisions
+        #region Collisions
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Portal")
+        private void OnTriggerEnter(Collider other)
         {
-            if (other.gameObject.name == "PortalMuseo")
+            if (other.gameObject.tag == "Portal")
             {
-                SceneManager.LoadScene("MuseoScene");
-            }
-            else if (other.gameObject.name == "PortalBosque")
-            {
-                SceneManager.LoadScene("BosqueScene");
+                if (other.gameObject.name == "PortalMuseo")
+                {
+                    SceneManager.LoadScene("MuseoScene");
+                }
+                else if (other.gameObject.name == "PortalBosque")
+                {
+                    SceneManager.LoadScene("BosqueScene");
+                }
             }
         }
-    }
-    #endregion
+        #endregion
 
+    }
 }
