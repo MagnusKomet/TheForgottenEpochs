b0918d0cb3235654688e554e1607ee32b42734c3
MagnusKomet <ehosta.dam@institutcampalans.net>
2025-03-06 15:29:57 +0100

    PlayerController attack fixes

--- Assets/Scripts/VisualInventoryController.cs
+++ Assets/Scripts/VisualInventoryController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class VisualInventoryController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject InventoryMenu;
+    private bool menuActivated;
+    //public ItemSlot[] itemSlot;
+    //public ItemSO[] itemSOs;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Inventory") && menuActivated)
+        {
+            Time.timeScale = 1;
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+        else if (Input.GetButtonDown("Inventory") && !menuActivated)
+        {
+            Time.timeScale = 0;
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+        }
     }
 }
