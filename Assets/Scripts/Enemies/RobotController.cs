3acc55dc5bdf70844dc9e195829f8ae2d5c6e23e
MagnusKomet <ehosta.dam@institutcampalans.net>
2025-04-07 15:23:49 +0200

    Robot AI changes

--- Assets/Scripts/Enemies/RobotController.cs
+++ Assets/Scripts/Enemies/RobotController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,16 @@ public class RobotController : EnemyController
     private Transform laserMount;
     [SerializeField]
     private Transform weapon;
+    int index;
+    public Transform pathParent;
+    Transform targetPoint;
+
+    public void Start()
+    {
+        // Inicialitza l'índex en el primer punt del camí i assigna el primer punt com a targetPoint
+        index = 0;
+        targetPoint = pathParent.GetChild(index);
+    }
 
     // Update is called once per frame
     private void Update()
@@ -54,21 +65,17 @@ public class RobotController : EnemyController
     //que vaya de punto a punto y que vaya dando vueltas el laser
     public override void Patroling()
     {
-        if (!walkPointSet)
+        // Mou l'objecte cap al punt objectiu
+        agent.SetDestination(targetPoint.position);
+
+
+        // Comprova si l'objecte ha arribat al punt objectiu i si és així, canvia el punt objectiu al següent
+        if (Vector3.Distance(transform.position, targetPoint.position) < 1f)
         {
-            SearchWalkPoint();
+            index++;
+            index %= pathParent.childCount;
+            targetPoint = pathParent.GetChild(index);
         }
-
-        if (walkPointSet)
-        {
-            agent.SetDestination(walkPoint);
-        }
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        // Walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f)
-            walkPointSet = false;
     }
 
     // hacer que compruebe si ve al jugador desde el laser
@@ -76,12 +83,28 @@ public class RobotController : EnemyController
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(laserMount.position, (player.position - transform.position).normalized, out hit, range))
+        if (Physics.Raycast(transform.position, (player.position - transform.position).normalized, out hit, range))
         {
-            Debug.DrawRay(laserMount.position, (player.position - transform.position).normalized * sightRange, Color.blue);
+            Debug.DrawRay(transform.position, (player.position - transform.position).normalized * sightRange, Color.blue);
             return hit.transform == player;
         }
 
         return false;
+    }
+
+    // Dibuixa el camí en l'editor per visualitzar-lo millor
+    void OnDrawGizmos()
+    {
+        Vector3 from;
+        Vector3 to;
+        // Recorre tots els fills de pathParent i dibuixa línies entre ells
+        for (int a = 0; a < pathParent.childCount; a++)
+        {
+            from = pathParent.GetChild(a).position;
+            to = pathParent.GetChild((a + 1) %
+            pathParent.childCount).position;
+            Gizmos.color = new Color(1, 0, 0);
+            Gizmos.DrawLine(from, to);
+        }
     }
 }
